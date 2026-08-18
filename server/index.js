56b46d73644845a644f480db086b3eae6576a69d
Thomas Park <thomaspark@uvic.ca>
2026-08-18 01:18:45 -0700

    initialized socket endpoints and http requests for clients

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const http = require("http");
 const app = express();
 const cors = require("cors");
 
-app.use(cors());
+const sock = require("ws")
+
+const server = http.createServer(app);
+const websock_serv = new sock.WebSocketServer({ server });
+
+app.use(cors()); //Express middleware
 app.use(express.json());
 
-app.listen(5000, () => {
+websock_serv.on("connection", (sock) => {
+    console.log("Client connection occurred")
+});
+
+server.listen(5000, () => {
     console.log("server has been initialized at port 5000")
 })
+
